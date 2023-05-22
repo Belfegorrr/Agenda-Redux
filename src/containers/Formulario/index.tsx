5b9552031fa79e1cead2dc8803b1d0c39003fbd7
Belfegorrr <pelichek.iago@gmail.com>
2023-05-22 00:26:21 -0300

    fix rotas e form

--- src/containers/Formulario/index.tsx
+++ src/containers/Formulario/index.tsx
@@ -3,16 +3,35 @@ import { useState, FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { BotaoSalvar, MainContainer } from '../../styles'
-import { Campo, Titulo } from '../../components/Contato/styles'
-import { Form, Opcoes, Opcao } from './styles'
+import { Titulo } from '../../components/Contato/styles'
+import { Form } from './styles'
 import { cadastrar } from '../../store/reducers/contatos'
 
 const Formulario = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [telefone, setTelefone] = useState(0)
+
+  const cadastrarTarefa = (evento: FormEvent) => {
+    evento.preventDefault()
+
+    dispatch(
+      cadastrar({
+        name,
+        email,
+        telefone
+      })
+    )
+    navigate('/')
+  }
+
   return (
     <MainContainer>
       <Titulo>Nova Contato</Titulo>
 
-      <Form>
+      <Form onSubmit={cadastrarTarefa}>
         <BotaoSalvar type="submit">Cadastrar</BotaoSalvar>
       </Form>
     </MainContainer>
